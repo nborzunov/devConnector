8201e4f837cf787d88ab115d37c9551861ebd6b2
NikolayBorzunov <forest3694@ya.ru>
2021-01-26 13:57:59 +0300

    Auth Middleware 1

--- routes/api/users.js
+++ routes/api/users.js
@@ -58,7 +58,7 @@ router.post('/',
 
         jwt.sign(
             payload, 
-            config.get('jwtToken'),
+            config.get('jwtSecretKey'),
             { expiresIn: 360000},
             (err, token) => {
                 if(err) throw err;
