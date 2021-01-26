4457edfaa09e3fd74162877500b1f04abcbad0e7
NikolayBorzunov <forest3694@ya.ru>
2021-01-26 15:11:59 +0300

    Profile route 1

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,28 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Profile route'));
+const auth = require('../../middleware/auth');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+
+router.get('/me', auth, async (req, res) => {
+    try{
+        const profile = await Profile
+        .findOne({user: req.user.id })
+        .populate(
+            'user', 
+            ['name', 'avatar']);
+
+        if(!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+
+        }
+    } catch(err) {
+        console.log(err.message);
+        res.atatus(500).send('Server Error');
+    }
+});
 
 module.exports = router;
