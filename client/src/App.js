9dc3648119329ddd084791fe0754f01355f71251
NikolayBorzunov <forest3694@ya.ru>
2021-01-31 12:32:11 +0300

    Created Alert Reducer

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,27 @@ import Navbar from './components/layout/Navbar.js';
 import Landing from './components/layout/Landing.js';
 import Login from './components/auth/Login.js';
 import Register from './components/auth/Register.js';
+// Redux
+import {Provider} from 'react-redux';
+import store from './store';
+
 import './App.css';
 
-const  App = () => 
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing}/>
-        <section className='container'>
-          <Switch>
-            <Route exact path='/register' component={Register}/>
-            <Route exact path='/login' component={Login}/>
-          </Switch>
-        </section>
-    </Fragment>
-  </Router>
+const  App = () =>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing}/>
+          <section className='container'>
+            <Switch>
+              <Route exact path='/register' component={Register}/>
+              <Route exact path='/login' component={Login}/>
+            </Switch>
+          </section>
+      </Fragment>
+    </Router>
+  </Provider>
 
 
 
--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -3,11 +3,12 @@ import { SET_ALERT, REMOVE_ALERT } from './actions/types';
 const initialState = [];
 
 export default function( state = initialState, action) {
-    switch(action.type){
+    const { type, payload } = action;
+    switch(type){
         case SET_ALERT:
-            return [ ...state, action.payload  ];
+            return [ ...state, payload  ];
         case REMOVE_ALERT:
-            return state.filter(alter => alert.id !== action.payload);
+            return state.filter(alert => alert.id !== payload);
         default:
             return state;
     }
