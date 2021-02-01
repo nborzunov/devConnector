cff99b58abbfc71f0ced47f1dbd8d99a9003d0c1
NikolayBorzunov <forest3694@ya.ru>
2021-02-01 11:16:55 +0300

    Get profile from api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -10,24 +10,22 @@ const User = require('../../models/User');
 const router = express.Router();
 
 router.get('/me', auth, async (req, res) => {
-    try{
-        const profile = await Profile
-        .findOne({user: req.user.id })
-        .populate(
-            'user', 
-            ['name', 'avatar']
-        );
-
-        if(!profile) {
-            return res.status(400).json({ msg: 'There is no profile for this user' });
-
-        }
-        res.json(profile);
-    } catch(err) {
-        console.log(err.message);
-        res.atatus(500).send('Server Error');
+    try {
+      const profile = await Profile.findOne({ user: req.user.id }).populate(
+        'user',
+        ['name', 'avatar']
+      );
+  
+      if (!profile) {
+        return res.status(400).json({ msg: 'There is no profile for this user' });
+      }
+  
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
     }
-});
+  });
 
 // Update main user data
 router.post(
