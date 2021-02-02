c35b5f755deeea4b103315ca9656b193c01cc746
NikolayBorzunov <forest3694@ya.ru>
2021-02-02 09:56:25 +0300

    Edit profile

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Login from './components/auth/Login.js';
 import Register from './components/auth/Register.js';
 import Alert from './components/layout/Alert.js';
 import Dashboard from './components/dashboard/Dashboard.js';
+import CreateProfile from './components/profile-forms/CreateProfile.js';
+import EditProfile from './components/profile-forms/EditProfile.js';
 import PrivateRoute from './components/routing/PrivateRoute.js';
 
 // Redux
@@ -38,6 +40,8 @@ const  App = () => {
                 <Route exact path='/register' component={Register}/>
                 <Route exact path='/login' component={Login}/>
                 <PrivateRoute exact path='/dashboard' component={Dashboard}/>
+                <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
+                <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
               </Switch>
             </section>
         </Fragment>
