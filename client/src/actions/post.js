8dec1fe4d91d2bc5854cfb80210e5ff5690e6b16
NikolayBorzunov <forest3694@ya.ru>
2021-02-04 13:31:49 +0300

    Like/Unlike/Remove Post features

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,7 +1,8 @@
-import {GET_POSTS, POST_ERROR} from './types';
+import {GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST} from './types';
 import axios from 'axios';
 import {setAlert} from './alert';
 
+// Get all posts
 export const getPosts = () => async dispatch => {
     try {
         const res = await axios.get('/api/posts');
@@ -16,5 +17,57 @@ export const getPosts = () => async dispatch => {
             payload: { msg: err.response.statusText, status: err.response.status }
           });
     }
-}
+};
 
+// Add Like
+export const addLike = postId => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/like/${postId}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { postId, likes: res.data }
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+          });
+    }
+};
+
+// Remove Like
+export const removeLike = postId => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/unlike/${postId}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { postId, likes: res.data }
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+          });
+    }
+};
+
+// Delete post
+export const deletePost = postId => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/posts/${postId}`);
+
+        dispatch({
+            type: DELETE_POST,
+            payload: postId
+        });
+
+        dispatch(setAlert('Post Removed', 'success'))
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+          });
+    }
+}
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import Moment from 'react-moment';
 import {connect} from 'react-redux';
+import {addLike, removeLike, deletePost} from '../../actions/post';
 
 const PostItem = ({ 
 
@@ -29,7 +30,7 @@ const PostItem = ({
             Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
         </p>
 
-        <button type='button' className='btn btn-light'>
+        <button onClick={e => addLike(_id)} type='button' className='btn btn-light'>
             <i className='fas fa-thumbs-up' />
             {' '}
             {likes.length > 0 && (
@@ -37,7 +38,7 @@ const PostItem = ({
             )} 
         </button>
 
-        <button type='button' className='btn btn-light'>
+        <button onClick={e => removeLike(_id)} type='button' className='btn btn-light'>
             <i className='fas fa-thumbs-down' />
         </button>
 
@@ -48,7 +49,7 @@ const PostItem = ({
         </Link>
 
         {!auth.loading && user === auth.user._id && (
-        <button type='button' className='btn btn-danger'>
+        <button onClick={e => deletePost(_id)} type='button' className='btn btn-danger'>
             <i className='fas fa-times' />
         </button>
 
@@ -60,6 +61,9 @@ const PostItem = ({
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired,
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired,
+    deletePost: PropTypes.func.isRequired,
 };
 const mapStateToProps = state => ({
     auth: state.auth
